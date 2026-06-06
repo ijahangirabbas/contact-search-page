1fec2e7bcdb620e32e494a7ba1a22ca1998ee74e
Jahangir Abbas <jahangirabbas278@gmail.com>
2026-06-06 10:29:38 +0500

    update file structure

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "public")));
 
 const CONTACTS_FILE = path.join(__dirname, "contacts.json");
 const SUBMISSIONS_FILE = path.join(__dirname, "submissions.json");
